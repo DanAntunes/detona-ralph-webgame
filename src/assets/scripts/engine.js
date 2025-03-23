335f5921bc9a689d976d2871ed865b7a0e3a3d0b
Soren <danilo-258@hotmail.com>
2025-03-23 08:04:50 -0300

    feat(engine.js): Atualiza lógica do jogo
    - Remove o console.log('Hellow World')
    - Adiciona objeto "state" contendo propriedades "view", "values" e "actions"
    - Implementa funções "countDown", "randomSquare", "playSound", "addListenerHitBox" e "main"
    - Configura timers para contagem regressiva e atualização dinâmica dos inimigos
    
    Essa atualização integra a lógica básica para manipulação do jogo, permitindo a atualização do placar e o controle do tempo.

--- src/assets/scripts/engine.js
+++ src/assets/scripts/engine.js
@@ -1,1 +1,67 @@
-console.log('Hellow World');
+const state = {
+  view:{
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+  },
+  values:{
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60,
+  },
+  actions:{
+    timerId: setInterval(randomSquare, 1000),
+    countDownTimerId: setInterval(countDown, 1000),
+  }
+};
+
+function countDown(){
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+  if (state.values.currentTime <= 0){
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+    // biome-ignore lint/style/useTemplate: <explanation>
+    alert("Game Over! O seu resultado foi: " + state.values.result);
+  }
+}
+
+function playSound(){
+  const audio = new Audio("./src/sounds/src_audios_hit.m4a");
+  audio.volume = 0.2;
+  audio.play();
+}
+
+function randomSquare(){
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  state.view.squares.forEach((square)=>{
+    square.classList.remove("enemy");
+  });
+
+  const randomNumber = Math.floor(Math.random() * 9);
+  const randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox(){
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown",() => {
+      if(square.id === state.values.hitPosition){
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+        playSound();
+      }
+    });
+  });
+}
+
+function main(){
+  addListenerHitBox();
+}
+
+main();
